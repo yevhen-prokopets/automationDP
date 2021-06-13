2d77e500b92f5c13808a261dd59327c5a760e87f
yevhen-prokopets <73821875+yevhen-prokopets@users.noreply.github.com>
2021-06-13 19:01:13 +0300

    last chance

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -3,14 +3,18 @@ import org.junit.   Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
 
 import java.util.concurrent.TimeUnit;
 
 
 public class SampleTest {
+    WebDriver webDriver;
     @Test
-    public void MoodleLogin() {
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "D:\\chromedriver_win32\\chromedriver.exe");
         WebDriver webDriver = new ChromeDriver();
+
         webDriver.get("https://moodle.chnu.edu.ua/login/index.php");
         webDriver.manage().timeouts().implicitlyWait(40, TimeUnit.MINUTES);
         webDriver.findElement(By.xpath("//*[@id=\"region-main\"]/div/div/div/div/div/div/div[2]/div[1]/div/a")).click();
@@ -18,6 +22,7 @@ public class SampleTest {
         webDriver.findElement(By.xpath("//*[@id=\"identifierNext\"]/div/button")).click();
         webDriver.findElement(By.name("password")).sendKeys("Toradora1");
         webDriver.findElement(By.xpath("//*[@id=\"passwordNext\"]/div/button")).click();
+
 
 
     }
