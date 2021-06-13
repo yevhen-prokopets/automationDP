20e6f05b08f1b8d63774f708ca732668ecc717cd
yevhen-prokopets <73821875+yevhen-prokopets@users.noreply.github.com>
2021-06-13 16:23:44 +0300

    new allure

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SampleTest {
 
-    @Test
+    @Test(timeout = 1000)
     public void MoodleLogin() {
         WebDriver webDriver = new ChromeDriver();
         webDriver.get("https://moodle.chnu.edu.ua/login/index.php");
