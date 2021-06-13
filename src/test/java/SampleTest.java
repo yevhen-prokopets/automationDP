3ea0ad28004ffa910f268cc53afb418af35039b0
yevhen-prokopets <73821875+yevhen-prokopets@users.noreply.github.com>
2021-06-13 12:22:37 +0300

    maven ver

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -11,7 +11,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class SampleTest {
-
     @Test
     public void MoodleLogin() {
         WebDriver webDriver = new ChromeDriver();
