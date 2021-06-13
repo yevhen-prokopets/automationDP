35bb881d9f855459516f325a18279e7ad9b9da20
yevhen-prokopets <73821875+yevhen-prokopets@users.noreply.github.com>
2021-06-13 14:35:35 +0300

    new allure

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -4,23 +4,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.util.concurrent.TimeUnit;
 
 
 
 public class SampleTest {
 
     @Test
-    public void MoodleLogin() throws InterruptedException {
+    public void MoodleLogin() {
         WebDriver webDriver = new ChromeDriver();
-        Thread.sleep(1000);
         webDriver.get("https://moodle.chnu.edu.ua/login/index.php");
-        webDriver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-        webDriver.findElement(By.xpath("//*[@id=\"region-main\"]/div/div/div/div/div/div/div[2]/div[1]/div/a")).click();
-        webDriver.findElement(By.xpath("//*[@id=\"identifierId\"]")).sendKeys("prokopets.yevhen@chnu.edu.ua");
-        webDriver.findElement(By.xpath("//*[@id=\"identifierNext\"]/div/button")).click();
-        webDriver.findElement(By.name("password")).sendKeys("Toradora1");
-        webDriver.findElement(By.xpath("//*[@id=\"passwordNext\"]/div/button")).click();
 
 
     }
