06c019ff5421b565ed26aa1dea89d1251e0f235d
yevhen-prokopets <73821875+yevhen-prokopets@users.noreply.github.com>
2021-06-13 19:06:26 +0300

    last chance

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -3,8 +3,6 @@ import org.junit.   Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeTest;
-
 import java.util.concurrent.TimeUnit;
 
 
